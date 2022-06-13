a7c2d668c1565f536879a90f751fd16f1f286c25
Ominox0 <99545746+Ominox0@users.noreply.github.com>
2022-06-13 23:28:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='AC',
     version="0.1",
     url='https://github.com/Ominox0/AC',
+    download_url = 'download link you saved',
     author='Fran Stanić',
     packages= ["AC"],
     zip_safe=False)
