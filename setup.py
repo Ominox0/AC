ee43e4616f92554ce8fb74cff3de1cb8ccb776ee
Ominox0 <99545746+Ominox0@users.noreply.github.com>
2022-06-13 20:51:51 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from AC import __version__
+
+setup(
+    name='AC',
+    version=__version__,
+
+    url='https://github.com/MichaelKim0407/tutorial-pip-package',
+    author='Fran Stanić',
+    author_email='franstanic24@gmail.com',
+
+    py_modules=['AC'],
+)
